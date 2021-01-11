2f2e9f35400b225563feb9b5be3cfc7fb192a968
Serkan Can <serkancantech@hotmail.com>
2021-01-11 19:03:48 +0300

    First commit

---
+++ src/Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+	public class Calculator
+	{
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        
+        public double Divide(int x, int y)
+        {
+            return Convert.ToDouble(x / y);
+        }
+    }
+}
---
+++ test/Calculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CalculatorApp.UnitTests
+{
+	public class CalculatorTests
+	{
+		[Fact]
+		public void Add_With1and0_ShouldReturn1()
+		{
+			Calculator calc = new Calculator();
+			var result = calc.Add(3, 5);
+			result.Should().NotBe(0); // Hatalı dogrulama
+		}
+
+		[Fact]
+		public void Subtract_With5and3_ShouldReturn2()
+		{
+			Calculator calc = new Calculator();
+			var result = calc.Subtract(5, 3);			
+			// Eksik doğrulama
+		}
+
+		[Fact]
+		public void Multiply_With5and3_ShouldReturn15()
+		{
+			Calculator calc = new Calculator();
+			var result = calc.Multiply(5, 3);
+			result.Should().Be(15);
+		}
+
+		[Theory]
+		[InlineData(1, 2)]
+		[InlineData(1, 0)]
+		public void Divide_WhenCalled_ShouldReturnResult(int x, int y)
+		{
+			Calculator calc = new Calculator();
+			try
+			{
+				var result = calc.Divide(x, y);
+			}catch(Exception ex)
+			{				
+				// Şimdilik çalışsın da sonra bakarız
+			}
+		}
+	}
+}
